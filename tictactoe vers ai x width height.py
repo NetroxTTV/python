5810794f107643c44b2317bf855701f90641a92d
NetroxTTV <147041723+NetroxTTV@users.noreply.github.com>
2023-11-30 21:06:10 +0100

    aa

--- tictactoe vers ai x width height.py
+++ tictactoe vers ai x width height.py
@@ -1,6 +1,15 @@
 import random
 import math
 
+def AskInt(message: str):
+    while True:
+        try:
+            value: int = int(input(message))
+            return value
+
+        except:
+            print("Please type a number and not a character")
+
 def ShowPlayfield(playfield, tab_lenght):
     for i in range(len(playfield)):
         print('\n')
@@ -9,8 +18,8 @@ def ShowPlayfield(playfield, tab_lenght):
     print('______________________')
     print('\n')     
 
-def CheckPlacement(playfield, user_choice, tab_lenght):
-    if 0 <= user_choice <= len(playfield)*tab_lenght:
+def IsEmptyCell(playfield, user_choice, tab_lenght):
+    if 0 <= user_choice < len(playfield)*len(playfield):
             x:int = user_choice % tab_lenght
             y:int = user_choice // tab_lenght
             if playfield[y][x] == ".":
@@ -18,15 +27,16 @@ def CheckPlacement(playfield, user_choice, tab_lenght):
     return False
 
 def PlayerMove(playfield, tab_lenght):
-    user_choice:int = int(input(f"Where do you want to place you symbole ? (between 1 and {len(playfield)}): "))
+    user_choice:int = AskInt(f"Where do you want to place you symbole ? (between 1 and {len(playfield)*len(playfield)}): ")
     user_choice-=1
-    while CheckPlacement(playfield, user_choice, tab_lenght) == False:
-        user_choice:int = int(input("Where do you want to place you symbole, do not put a symobole on a taken area ? : "))
+    while IsEmptyCell(playfield, user_choice, tab_lenght) == False:
+        user_choice:int = AskInt("Where do you want to place you symbole, do not put a symobole on a taken area ? : ")
+        user_choice-=1
     x:int = user_choice % tab_lenght
     y:int = user_choice // tab_lenght
     playfield[y][x] = "X"
 
-def GetPlayfieldLines(playfield, tab_lenght):
+def GetPlayfieldWinCombination(playfield, tab_lenght):
     lines: list[list[str]] = []
     # DIAGS
 
@@ -41,56 +51,122 @@ def GetPlayfieldLines(playfield, tab_lenght):
     lines.append(diag2)
 
     # Lines
-
-    line1:list[str] = []
-    line2:list[str] = []
-    line3:list[str] = []
-    line4:list[str] = []
-
+    
     for i in range(len(playfield)):
-        line1.append(i)
-    lines.append(line1)
-
-    for i in range(len(playfield)):
-        line2.append(i + len(playfield))
-    lines.append(line2)
-
-    for i in range(len(playfield)):
-        line3.append(i + 2*len(playfield))
-    lines.append(line3)
-
-    for i in range(len(playfield)):
-        line4.append(i + 3*len(playfield))
-    lines.append(line4)
+        line_d:list[str] = []
+        for j in range(len(playfield)):
+            line_d.append(j + i*len(playfield))
+        lines.append(line_d)
 
     # COL
-
-    col1:list[str] = []
-    col2:list[str] = []
-    col3:list[str] = []
-    col4:list[str] = []
-
-    for i in range(len(playfield)):
-        col1.append(i*len(playfield))
-    lines.append(col1)
-
-    for i in range(len(playfield)):
-        col2.append(i*len(playfield) + 1)
-    lines.append(col2)
-
-    for i in range(len(playfield)):
-        col3.append(i*len(playfield)+ 2)
-    lines.append(col3)
-
-    for i in range(len(playfield)):
-        col4.append(i*len(playfield) + 3)
-    lines.append(col4)
-    return lines
     
+    for i in range(len(playfield)):
+        col:list[str] = []
+        for x in range(len(playfield)):
+            col.append(x*len(playfield) + i)
+        lines.append(col)
+    return lines
 
 def GetIaMove(playfield:list, lines, letters:list = ["O", "X"]):
     for k in range(len(letters)):
-        for i in range(len(lines)):
+        for i in range(len(playfield)):
+            for j in range(len(playfield)):
+                check = IsEmptyCell(playfield, i*len(playfield) + j, len(playfield))
+                if check == True:
+                    playfield[i][j] = letters[k]
+                    res = TestWin(playfield, lines, letters[k])
+                
+                    if res == True:
+                        playfield[i][j] = "."
+                        return i*len(playfield) + j
+                
+                    playfield[i][j] = "."
+    num:int = random.randint(0,len(playfield)*len(playfield))
+    return num
+
+def RandomPlacement(playfield):       
+    num:int = random.randint(0,len(playfield)*len(playfield))
+    return num
+
+def IaMoveInitializer(playfield, tab_lenght, lines):
+    ia:int = GetIaMove(playfield, lines)
+
+    while IsEmptyCell(playfield, ia, tab_lenght) == False:
+        ia = RandomPlacement(playfield)
+        
+    x:int = ia % tab_lenght
+    y:int = ia // tab_lenght
+
+    playfield[y][x] = "O"
+
+def TestWin(playfield, lines, letter) -> bool:  
+    for i in range(len(lines)):
+        symbol_aligned_count = 0  
+        for j in range(len(lines[i])):
+            x:int = lines[i][j] % len(playfield)
+            y:int = lines[i][j] // len(playfield)
+            if playfield[y][x] == letter != ".":
+                symbol_aligned_count += 1
+        if symbol_aligned_count == len(playfield):
+            return True
+    return False
+    
+def Init():
+    playfield:list[list[str]] = []
+    tab_lenght: int = AskInt("What size of TicTacToe do you want ? : ")
+    for i in range(0, tab_lenght):
+        line: list[str] = []
+        for j in range(0, tab_lenght):
+            line.append(".")
+        playfield.append(line)
+    return playfield
+
+def StartGame():
+    playfield = Init()
+    tab_lenght = len(playfield)
+    winner:str = " "
+    run:bool = True
+    while run == True:
+        PlayerMove(playfield, tab_lenght)
+        ShowPlayfield(playfield, tab_lenght)
+        lines:list[list[str]] = GetPlayfieldWinCombination(playfield, tab_lenght)
+        if TestWin(playfield, lines, "X") == True:   
+            winner = "Player"
+            run = False
+        else:
+            counter = 0
+            for i in range(len(playfield)):
+                for j in range(len(playfield)):
+                    if playfield[i][j] != ".":
+                        counter += 1
+            if counter == len(playfield)*len(playfield):
+                print(f"It's a Tie !")
+                return False
+            IaMoveInitializer(playfield, tab_lenght, lines)
+            ShowPlayfield(playfield, tab_lenght)
+            if TestWin(playfield, lines, "O") == True: 
+                winner = "Bot"
+                run = False
+
+    print(f"The {winner} won the game !")
+    
+StartGame()
+
+
+'''for i in range(len(playfield)):
+            for j in range(len(playfield)):
+                check = CheckPlacement(playfield, playfield[i][j], len(playfield))
+                if check == True:
+                    playfield[i][j] = letters[k]
+                    res = TestWin(playfield, lines, letters[k])
+                    if res == True:
+                        playfield[i][j] = "."
+                        return playfield[i][j]
+                        
+                        
+                        
+                        
+    for i in range(len(lines)):
             symbol_aligned_count:int = 0
             dot_count:int = 0
             for j in range(len(lines[i])):
@@ -102,71 +178,4 @@ def GetIaMove(playfield:list, lines, letters:list = ["O", "X"]):
                     IaPlace:int = y*len(playfield) + x
                     dot_count +=1
             if symbol_aligned_count == len(playfield)-1 and dot_count == 1:
-                return IaPlace
-    num:int = random.randint(0,len(playfield)*len(playfield))
-    print(num)
-    return num
-
-def IaMoveInitializer(playfield, tab_lenght, lines):
-    ia:int = GetIaMove(playfield, lines)
-
-    while CheckPlacement(playfield, ia, tab_lenght) == False:
-        counter = 0
-        ia = GetIaMove(playfield, lines)
-        
-    x:int = ia % tab_lenght
-    y:int = ia // tab_lenght
-
-    playfield[y][x] = "O"
-
-def TestWin(playfield, lines, letter:str = ["O", "X"]) -> bool:  
-    print("test win")
-    for k in range(len(letter)):
-        for i in range(len(lines)):
-            symbol_aligned_count = 0  
-            for j in range(len(lines[i])):
-                    x:int = lines[i][j] % len(playfield)
-                    y:int = lines[i][j] // len(playfield)
-                    if playfield[y][x] == letter[k] != ".":
-                        symbol_aligned_count += 1
-            if symbol_aligned_count == len(playfield):
-                return True
-    return False
-    
-
-def StartGame():
-    playfield:list[list[str]] = []
-    tab_lenght: int = int(input("What size of TicTacToe do you want ? : "))
-    for i in range(0, tab_lenght):
-        line: list[str] = []
-        for j in range(0, tab_lenght):
-            line.append(".")
-        playfield.append(line)
-    print(playfield)
-    winner:str = " "
-    run:bool = True
-    while run == True:
-        PlayerMove(playfield, tab_lenght)
-        ShowPlayfield(playfield, tab_lenght)
-        lines = GetPlayfieldLines(playfield, tab_lenght)
-        if TestWin(playfield, lines) == True:   
-            winner = "Player"
-            run = False
-        else:
-            IaMoveInitializer(playfield, tab_lenght, lines)
-            ShowPlayfield(playfield, tab_lenght)
-            if TestWin(playfield, lines) == True: 
-                winner = "Bot"
-                run = False
-        counter = 0
-        for i in range(len(playfield)):
-            if playfield[i] != ".":
-                counter += 1
-            if counter == 9:
-                print(f"It's a Tie !")
-                run = False
-                return
-
-    print(f"The {winner} won the game !")
-    
-StartGame()
+                return IaPlace'''
--- wordle.py
+++ wordle.py
@@ -1,37 +1,34 @@
 import random
 
 def jeu(tentative):
-    tab = ["a", "meow", "grrrr", "imagine", "osu"]
-    pew = random.randint(1, len(tab))
-    mot_cache = tab[pew]
-    print(mot_cache)
-    lg_mot = len(mot_cache)
+    tab = ["trace", "creer", "jouer", "lucas", "amour", "avoir", "monde", "poule", "foule"]
+    word = random.randint(1, len(tab))
+    hidden_word = tab[word]
+    lg_mot = len(hidden_word)
     while tentative > 0:
         mot_joueur = input("Donne un mot de longueur " + str(lg_mot) + " : ")
-        while len(mot_joueur) > len(mot_cache) or len(mot_joueur) < len(mot_cache):
+        while len(mot_joueur) > len(hidden_word) or len(mot_joueur) < len(hidden_word):
             print("mot n'a pas ", lg_mot, "caracteres")
             mot_joueur = input("Donne un mot de longueur " + str(lg_mot) + " : ")
     
-        a = 0
-        b = 0
-        temp_base = []
+        print_word = []
         for i in range(lg_mot):    
-            if mot_cache[i] == mot_joueur[i]:
-                a = a+1
-            elif mot_cache[i] in mot_joueur:
-                b = b + 1
-                
-                    
-        if a == lg_mot:
-                print("tu as trouvé le mot")
-                return
-        else:
-                print("tu as", a, "lettre de a la bonne place")
-                print("tu as", b, "lettre de a la mauvaise place")
-                print("tu as", tentative-1 , "tentative restantes")
-                tentative = tentative - 1
-    print("perdu")
-    return
+            if hidden_word[i] == mot_joueur[i]:
+                print_word.append("\033[92m" + mot_joueur[i] + "\033[0m ")
+            elif mot_joueur[i] in hidden_word:
+                print_word.append("\033[91m" + mot_joueur[i] + "\033[0m ")
+            else:
+                print_word.append("\033[90m" + mot_joueur[i] + "\033[0m ")
+        word_a = ""
+        for i in range(len(print_word)):
+            word_a = word_a + print_word[i]            
+        
+        tentative -= 1
+        print(f"{word_a}\n Tu as {tentative} tentatives restantes\n")
 
+        if hidden_word == mot_joueur:
+            print("you won")
+            return 
+    return
 tentative = 10
 jeu(tentative)
