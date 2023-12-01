e3d433cf617fe6772128f5afe064092f06931afd
NetroxTTV <147041723+NetroxTTV@users.noreply.github.com>
2023-12-01 11:36:37 +0100

    A

--- plus_ou_moins.py
+++ plus_ou_moins.py
@@ -1,62 +1,58 @@
 import random
 
-def CheckIfINT(nb,  chiffre_un, chiffre_de):
-    try:
-        int(nb)
-        it_is:bool = True
-        nb = int(nb)
-    except ValueError:
-        it_is:bool = False
-        print("donne un chiffre et non un chaine de charactere")
-    while it_is == False:
-        nb:str = input("Donne un bon nombre stp : ")
+def AskInt(message: str, min: int, max: int):
+    while True:
         try:
-            int(nb)
-            it_is:bool = True
-            nb = int(nb)
-        except ValueError:
-            it_is:bool = False
-            print("donne un chiffre et non un chaine de charactere")
-    
-    if nb > chiffre_de:
-        while nb > chiffre_de or nb < chiffre_un:
-            print("Nombre pas comprit entre 1 et 100, redonne un nombre")
-            nb = int(input("Donne ton nombre : "))
+            value: int = int(input(message))
 
-    return nb
+            if value >= min and value <= max:
+                return value
 
+            print("Not in range")
 
-def Game(nb, essai,  chiffre_un, chiffre_de):
-    while essai != 10:
-        nb_joueur:str = input("Donne ton nombre : ")
-        nb_joueur:str = CheckIfINT(nb_joueur,  chiffre_un, chiffre_de)
+        except:
+            print("Please type a number and not a character") 
+ 
+
+def jeu(nb, tries,  min, max):
+    while tries != 10:
+        nb_joueur = AskInt("Please type a number : ", min, max)
+
         if nb_joueur>nb:
-            print("ton chiifre est tro grand")
-            essai = essai + 1
+            print("My number is smaller")
+            tries = tries + 1
         elif nb_joueur<nb:
-            print("ton chiifre est tro petit")
-            essai = essai + 1
+            print("My number is bigger")
+            tries = tries + 1
         else:
-            rep:str = input("gg a toi bg, tu veux recommencer une partie ? (y/n) ")
-            if rep == "y" or rep == "yes":
-                Start()
+            rep:str = input("Good job, do you want to replay ? (y/n) ")
+            if rep == "y":
+                start()
             else:
                 print("ok")
-                essai = 10
                 return
+            
+    print("You Lost")
+    rep:str = input("Good job, do you want to replay ? : ")
+    if rep == "y":
+        start()
+    else:
+        print("See ya")
+        return 
 
-def Start():    
-    ask:str = input("Veut-tu fixer des bornes du nombre a deviner ? (y/n)")
-    essai:int = 0
-    chiffre_un:int = 1
-    chiffre_de:int = 100
+def start():
+    ask:str = input("Do you want to To fix the range ? (y/n)")
+    tries:int = 0
+    min:int = 1
+    max:int = 100
     if ask == "y":
-        chiffre_un:int = int(input("Donne ton 1er chiffre : "))
-        chiffre_de:int = int(input("Donne ton 2e chiffre : "))
-        nb = random.randint(chiffre_un,chiffre_de)
+        min:int = int(input("Type your number : "))
+        max:int = int(input("Type your number : "))
+        nb = random.randint(min,max)
+        print(nb)
     else:
         nb:int = random.randint(1,100)
 
-    Game(nb, essai, chiffre_un, chiffre_de)
+    jeu(nb, tries, min, max)
 
-Start()
+start()
